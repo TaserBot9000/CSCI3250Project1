cf4447c073de24000aaed213ac9f65f7abd8273a
ciphernautic <165357883+ciphernautic@users.noreply.github.com>
2026-09-15 12:57:19 -0400

    Add comments for TCP port scanning functionality
    Added comments for future implementation of TCP port scanning.

--- myNmap.py
+++ myNmap.py
@@ -1,2 +1,10 @@
 import sys #access to interpreter
 import socket #communicate with the network
+
+#Get TCP open ports on the target
+
+#Check that the target was input
+
+#Check that target IP can be reached
+
+#Output TCP open ports on target
