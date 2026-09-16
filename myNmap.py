07bf4af382ddcf7b0962a06bb06264dce149c09d
ciphernautic <165357883+ciphernautic@users.noreply.github.com>
2026-09-16 19:33:21 -0400

    Improve target IP input handling
    Refactor input handling to store target IP.  Better else path needed without input.

--- myNmap.py
+++ myNmap.py
@@ -4,13 +4,16 @@ import socket #communicate with the network
 # Check that the target was input
 argument_input = len(sys.argv) #total length of argument vector
 
-if argument_input > 1:
-    print(f"Input: {sys.argv[1]}") #temporary test
+if argument_input > 1: # Ensure there was input
+    # Get target IP from cmd line
+    target_ip = sys.argv[1] #
+    print(f"Input: {target_ip}") 
+    
 else:
-    print("Nothing was input.")
-
-# Get target IP from cmd line
+    print("Nothing was input.") 
 
 # Scan the TCP ports
 
 # Check if target IP can be reached
+
+# Output TCP open ports on target
