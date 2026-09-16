ccde75c9a6f9c8748148c93539a2fcc0b11a2065
ciphernautic <165357883+ciphernautic@users.noreply.github.com>
2026-09-16 19:29:27 -0400

    argument input check with temporary statement
    Added input argument check and print statement.

--- myNmap.py
+++ myNmap.py
@@ -1,12 +1,16 @@
-import sys #access to command line
+import sys #access to command line arguments
 import socket #communicate with the network
 
 # Check that the target was input
+argument_input = len(sys.argv) #total length of argument vector
+
+if argument_input > 1:
+    print(f"Input: {sys.argv[1]}") #temporary test
+else:
+    print("Nothing was input.")
 
 # Get target IP from cmd line
 
 # Scan the TCP ports
 
 # Check if target IP can be reached
-
-# Output TCP open ports on target
