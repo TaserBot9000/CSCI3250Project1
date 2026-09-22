e7b8b5ae8bc29853c0b55261a463c205ee2aabcc
TaserBot9000 <130939065+TaserBot9000@users.noreply.github.com>
2026-09-22 17:31:06 -0400

    Implement IP reachability check before port scan
    Added functionality to check if the target IP is reachable before scanning for open ports.

--- myNmap.py
+++ myNmap.py
@@ -22,20 +22,47 @@ except ValueError: #handle invalid IP address
     print(f"{target_ip} is not an IP address.")
     sys.exit() 
 
-#Attempt a TCP connection
-for current_port in range(1, 23): #temporary range, change later
+#see if ip is reachable
+reachable = False
+examplePort = 80
+checkReach = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+checkReach.settimeout(1.0)
+ 
+try:
+    checkReach.connect((target_ip, examplePort))
+    reachable = True #port open :) 
+ 
+except ConnectionRefusedError:
+    reachable = True #port closed, host open :|
+ 
+except OSError: 
+    reachable = False #port not open :(
+ 
+finally:
+    checkReach.close()
+
+if not reachable:
+    print(f"{target_ip} is not reachable")
+    sys.exit()
+
+
+#try to connect
+openPorts = 0
+
+for current_port in range(1, 81): 
     port_scan = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     port_scan.settimeout(1.0)
-
     try:
         port_scan.connect((target_ip, current_port))
         print(f"Port: {current_port} is listening.")
+        openPorts += 1
 
     except OSError: #If connection fails dont print it.
         pass
 
     port_scan.close()
 
-# Check if target IP can be reached
 
-# Output TCP open ports on target
+# print how many open ports
+print(f"\nScan finished! {openPorts} open ports found.")
+
