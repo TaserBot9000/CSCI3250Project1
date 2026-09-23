f98780e5bcd22187cb72fcf719a220b87e6c8511
ciphernautic <165357883+ciphernautic@users.noreply.github.com>
2026-09-22 22:55:26 -0400

    Update import comment for subprocess module

--- myNmap.py
+++ myNmap.py
@@ -1,7 +1,7 @@
 import sys #access to command line arguments
 import ipaddress #ipv4 and ipv6 library
 import socket #communicate with the network
-import subprocess #run commands
+import subprocess #run system commands
 
 # Check that the target was input
 argument_input = len(sys.argv) #total length of argument vector
